ebad16dabf1c8ee558d330f4335403df324d14cd
arpit-yadav01 <arpityadav170402@gmail.com>
2026-02-17 16:17:24 +0530

    db connection

---
+++ backend/app/db/mongo.py
@@ -0,0 +1,8 @@
+from motor.motor_asyncio import AsyncIOMotorClient
+from app.core.config import settings
+
+client = AsyncIOMotorClient(settings.MONGO_URL)
+db = client[settings.DB_NAME]
+
+users_collection = db["users"]
+trips_collection = db["trips"]
