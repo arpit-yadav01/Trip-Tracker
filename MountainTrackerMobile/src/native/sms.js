f6d24e6fa2d82a5248cb1c2e0652e5345470a367
arpit-yadav01 <arpityadav170402@gmail.com>
2026-02-20 14:21:29 +0530

    sms msg file

--- MountainTrackerMobile/src/native/sms.js
+++ MountainTrackerMobile/src/native/sms.js
@@ -5,7 +5,7 @@ const { SmsModule } = NativeModules;
 export async function sendEmergencySMS(phone, message) {
   try {
     const result = await SmsModule.sendSMS(phone, message);
-    console.log(result);
+    console.log("SMS Sent:", result);
   } catch (error) {
     console.log("SMS Error:", error);
   }
